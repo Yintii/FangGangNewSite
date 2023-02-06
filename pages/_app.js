3c820923d83d2cc3a7ece26f068de2f4bc9d46f5
yintii <keleheart1@gmail.com>
2023-02-05 20:21:53 -0800

    fully integrated next.js for optimal SEO performance for fang gang

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
 import '@/styles/globals.css'
+import { config, library } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+config.autoAddCss = false
+
+
+library.add(faTwitter);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,123 +1,161 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+import { useEffect } from 'react'
+import { useInView } from 'react-intersection-observer'
+
+
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+
+    const NavComponent = () => {
+        return(
+            <nav>
+                <div id="brand">
+                    <img
+                        src={"./images/fangganglogo.png"}
+                        alt="FangGang Copyright Awoo Studios"
+                        width="353px"
+                    />
+                    <ul id="nav-menu">
+                        <li>FANG GANG</li>
+                        <li>PXLFANGS</li>
+                        <li>WLDFANGS</li>
+                    </ul>
+                </div>
+                <div id="nav-buttons">
+                    <button>
+                        Join
+                    </button>
+                    <button>
+                        <FontAwesomeIcon icon={faTwitter} />
+                    </button>
+                </div>
+            </nav>
+        )
+    }
+    const IntroSection = () => {
+        return(
+            <section id="intro">
+                <div id="intro-content">
+                    <h1>THE BACKBONE OF <span className="gold">CREATIVE CULTURE</span>.</h1>
+                    <span>Discover the character brand straight from New Fang City.</span>
+                </div>
+                <img id="groupPhoto" src={'images/groupphoto.png'} width="1179px" />
+            </section>
+        )
+    }
+    const MeetSection = () => {
+
+        const [meetRef, meetInView] = useInView({ threshold: 0 })
+        const [fangAvatarRef, fangAvatarRefInView] = useInView({threshold: 0});
+        const [newFangCityRef, newFangCityRefInView] = useInView({threshold: 0});
+
+        useEffect(() => {
+            meetInView ? document.querySelector('#meet-copy').classList.add('slideInLeft') : null;
+        }, [meetInView]);
+
+        useEffect(() => {
+            fangAvatarRefInView ? document.querySelector(".fang-img-area").classList.add('zoomingIn') : null;
+        }, [fangAvatarRefInView])
+
+        useEffect(() => {
+            newFangCityRefInView ? document.querySelector("#newFangCity-img").classList.add('slideInRight') : null;
+        }, [newFangCityRefInView])
+
+        return(
+            <section id="meet">
+                <div>
+                    <div id="meet-copy" ref={meetRef}  className="purple-bg">
+                        <div id="meet-wrap">
+                            <h2>MEET THE <span className="gold">FANG GANG</span>.</h2>
+                            <button>VISIT</button>
+                            <p>Join the Fangsters on the streets of New Fang City, our very own microverse where we connect, enjoy special
+                                events,
+                                hang out and spend the $AWOO ecosystem token.</p>
+                        </div>
+                    </div>
+                    <div className="fang-img-area" ref={fangAvatarRef}>
+                        <img src={'./images/mascot.png'} width="196px" height="179px" />
+                    </div>
+                </div>
+                <img id="newFangCity-img" ref={newFangCityRef} src={"./images/newFangCity.png"} />
+            </section>
+        )
+    }
+    const PxlFangsSection = () => {
+
+        const [pxlFangAvatarRef, pxlFangAvatarRefInView] = useInView({threshold:0});
+
+        useEffect(()=>{
+            pxlFangAvatarRefInView ? document.querySelector('.px-fang-img-area').classList.add('zoomingIn') : null
+        }, [pxlFangAvatarRefInView]);
+
+        return(
+            <section id="pxlfangs">
+                <div id="px-top">
+                    <div id="pxl-copy">
+                        <h1>RUN WITH THE <span className="gold">PXLFANGS</span></h1>
+                        <div id="pxl-buttons">
+                            <button className="gold-bg">JOIN</button>
+                            <button className="gold-bg">CLAIM</button>
+                            <button className="gold-bg">PLAY</button>
+                        </div>
+                        <p>
+                            To expand the Fangverse and enter the gaming territory we introduced PxlFangs, a first of its kind pixelated
+                            side
+                            collection. Every Fangster NFT has a PxlFangster attached, to be claimed for free.
+                        </p>
+                        <p>
+                            With these PxlFangs you’ll
+                            get access
+                            to exclusive skins in some of the most popular metaverse projects, passive $AWOO income and much, much more.
+                        </p>
+                        <p>
+                            To accompany this expansion we also released an arcade game; Fang Runner.
+                        </p>
+                        <div id="px-bottom">
+                            <img id="city-img" src="./images/pxlrunner.png" width="610px" />
+                            <img id="game-case" src="./images/game-case.png" width="246px" />
+                        </div>
+                    </div>
+                    <div className="px-fang-img-area" ref={pxlFangAvatarRef}>
+                        <img src="./images/pxlfang.png" width="196px" height="179px" alt="pxlfang mascot" />
+                    </div>
+                </div>
+                <div id="dropdowns">
+                    <div id="claim">
+                        <div id="claim-header">
+                            <h2 className="black">CLAIM YOUR PXLFANGSTER.</h2>
+                            <input type="text" placeholder="8888" />
+                            <input type="button" value="CHECK CLAIM STATUS" />
+                        </div>
+                        <div id="current_fangs">
+                        </div>
+                    </div>
+                    <div id="play" hidden>
+
+                    </div>
+                </div>
+            </section>
+        )
+    }
+
   return (
-    <>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
+<>
+    <NavComponent />
+    <main>
+        <IntroSection />
+        <MeetSection />
+        <PxlFangsSection />
+    </main>
+</>
   )
 }
