5f550a9188c98df5066bfc65549ee54897acff36
yintii <keleheart1@gmail.com>
2023-03-11 12:13:06 -0800

    made the nav mobile responsive and toggleable

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect,useState } from 'react';
 import Link from 'next/link';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const NavComponent = () => {
+
+    const [revealed, setRevealed] = useState(false);
+    
+    const navRef = useRef();
+
+    function handleRevealNav(){
+        setRevealed(!revealed)
+    }
+
+
+    useEffect(() => {
+        if(window.innerWidth <= 767){
+            console.log("the ref", navRef)
+        }
+    }, [window.innerWidth]);
+
     return (
         <nav>
             <img
@@ -11,8 +27,12 @@ const NavComponent = () => {
                 src={"./images/logo_fanggang_navbar.png"}
                 alt="FangGang Copyright Awoo Studios"
             />
-            <img id='hamburger-menu' src='./images/menu.png' />
-            <div id='navigation'>
+            <img 
+                id='hamburger-menu' 
+                src={revealed ? './images/xmenu.png' : './images/menu.png'}
+                onClick={() => handleRevealNav()}
+            />     
+            <div id='desk-navigation' >
                 <div id="nav-menu">
                     <Link href="#meet">FANG GANG</Link>
                     <Link href="#pxlfangs">PXLFANGS</Link>
@@ -23,9 +43,21 @@ const NavComponent = () => {
                     <button>
                         JOIN
                     </button>
+                    <a href="https://twitter.com/FangGangNFT" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+                </div>
+            </div>
+            <div id='mobile-navigation' className={revealed ? '' : 'hidden'} >
+                <div id="nav-menu">
+                    <Link href="#meet">FANG GANG</Link>
+                    <Link href="#pxlfangs">PXLFANGS</Link>
+                    <Link href="#gear">WLDFNGS</Link>
+                </div>
+
+                <div id="nav-buttons">
                     <button>
-                        <FontAwesomeIcon icon={faTwitter} />
+                        JOIN
                     </button>
+                    <a href="https://twitter.com/FangGangNFT" target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
                 </div>
             </div>
         </nav>
