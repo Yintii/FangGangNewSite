0a865c18200650caa0bddc7571b84377332a9d91
yintii <keleheart1@gmail.com>
2023-03-26 19:27:07 -0700

    nearly finished with edits

--- components/OurPartners.js
+++ components/OurPartners.js
@@ -9,7 +9,7 @@ const OurPartners = () => {
             <h2>OUR PARTNERS.</h2>
             <div id="partners-area">
                 <Image src="/images/nord.svg" width={180} height={41} />
-                <Image src="/images/boson.svg" width={188} height={41}/>
+                <Image src="/images/boson.png" width={188} height={41}/>
                 <Image src="/images/nfc.svg" width={107} height={41} />
             </div>
         </div>
